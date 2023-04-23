5ebbf3d4734cf516dd2c07ef908100a4686fc943
Joseph Stateson <josephy@stateson.net>
2023-04-23 08:59:21 -0500

    bugfix

--- Enable-R4-PCB.cmd
+++ Enable-R4-PCB.cmd
@@ -1,4 +1,4 @@
 rem put this script and the 64 bit version of DevManView in the dell folder and run as admin
-C:\Users\josep\Downloads\dell>DevManView.exe /disable "USB\VID_0000&PID_0002\6&566274C&0&1"
+C:\Users\josep\Downloads\dell\DevManView.exe /disable "USB\VID_0000&PID_0002\6&566274C&0&1"
 pause
-C:\Users\josep\Downloads\dell>DevManView.exe /enable "USB\VID_0000&PID_0002\6&566274C&0&1"
+C:\Users\josep\Downloads\dell\DevManView.exe /enable "USB\VID_0000&PID_0002\6&566274C&0&1"
